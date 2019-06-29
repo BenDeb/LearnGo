9f309e6f1d611b7102bfeddb2f061c008eb4d2dd
BenDeb <bendebsx@gmail.com>
2019-06-29 13:32:13 +0200

    use struct for problems; read all CSV file instead of line by line; general improvements

--- gophercises/quiz/main.go
+++ gophercises/quiz/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
-	"log"
+	//"io"
+	//"log"
 	"os"
 	"strings"
 )
@@ -18,19 +18,20 @@ func main() {
 	//Load the CSV file with os.Open()
 	csvFile, err := os.Open(*filenamePtr)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *filenamePtr))
 	}
 	//Close the file at the end of the function
 	defer csvFile.Close()
 
 	//Create a new reader
 	csvr := csv.NewReader(csvFile)
-	score := 0
-	problemNb := 0
+	//problemNb := 0
 	//new reader for stdin input
-	reader := bufio.NewReader(os.Stdin)
+	//reader := bufio.NewReader(os.Stdin)
 
-	for {
+	//Cette première méthode ouvre une ligne du CSV à la fois
+	/*for {
 		row, err := csvr.Read()
 		if err == io.EOF {
 			break
@@ -43,6 +44,48 @@ func main() {
 		}
 
 		problemNb++
+	}*/
+	//Cette deuxième méthode ouvre tout le fichier en mémoire
+	rows, err := csvr.ReadAll()
+	if err != nil {
+		exit("Failzd to parse the provided CSV file")
 	}
-	fmt.Println("Final score:", score, "out of ", problemNb)
+
+	problems := parseRows(rows)
+	score := checkAnswer(problems)
+	fmt.Println("Final score:", score, "out of", len(problems))
+}
+
+func checkAnswer(problems []problem) int {
+	score := 0
+	for i, problem := range problems {
+		fmt.Printf("Problem number %d: %s\n", i+1, problem.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == problem.a {
+			score++
+		}
+	}
+	return score
+}
+
+func parseRows(rows [][]string) []problem {
+	ret := make([]problem, len(rows))
+	for i, row := range rows {
+		ret[i] = problem{
+			q: row[0],
+			a: strings.TrimSpace(row[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
